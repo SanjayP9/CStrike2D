17e1faf7159561c7a29b21e9725c0b0d07cfc2a5
SanjayP9 <paraboosanjay@gmail.com>
2015-12-16 10:15:06 -0500

    Made Raycasting constructor

---
+++ cstrike2d/cstrike2d/RayCast.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cstrike2d
+{
+    class RayCast
+    {
+
+        public RayCast(Vector2 emitLocal, float angle)
+        {
+
+        }
+    }
+}
