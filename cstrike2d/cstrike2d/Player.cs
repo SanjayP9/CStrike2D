0f29ed42ad65a07a9940a619a6b349b9113f5477
DevHalo <devhalo@hotmail.com>
2015-12-25 03:36:36 -0500

    Content Update

---
+++ cstrike2d/cstrike2d/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CStrike2D;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace cstrike2d
+{
+    public class Player : Entity
+    {
+        public override int DrawOrder { get; protected set; }
+        public override Vector2 Position { get; protected set; }
+        public override Rectangle Dimensions { get; protected set; }
+
+        public Player()
+        {
+            
+        }
+
+        public override void Update(float gameTime)
+        { 
+
+        }
+
+        public override void Draw(SpriteBatch sb, Assets assets)
+        {
+        }
+    }
+}
---
+++ cstrike2d/cstrike2d/UIManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CStrike2D;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace cstrike2d
+{
+    public class UIManager
+    {
+        List<GUIComponent> guiComponents = new List<GUIComponent>();
+
+        public void AddComponent(GUIComponent component)
+        {
+            guiComponents.Add(component);
+        }
+
+        public void Update(float gameTime)
+        {
+            foreach (GUIComponent component in guiComponents)
+            {
+                component.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch sb, Assets assets)
+        {
+            foreach (GUIComponent component in guiComponents)
+            {
+                component.Draw(sb, assets);
+            }
+        }
+
+        /// <summary>
+        /// Checks if a button was clicked
+        /// </summary>
+        /// <param name="identifier"></param>
+        public bool Clicked(InputManager input, string identifier)
+        {
+            Button button = (Button)guiComponents.Find(btn => btn.Identifier == identifier);
+
+            return button != null && button.Clicked(input);
+        }
+
+        public bool Hover(InputManager input, string identifier)
+        {
+            Button button = (Button) guiComponents.Find(btn => btn.Identifier == identifier);
+
+            return button != null && button.Hover(input);
+        }
+
+        /// <summary>
+        /// Shows the specified component
+        /// </summary>
+        /// <param name="identifier"></param>
+        public void Show(string identifier)
+        {
+            GUIComponent component = guiComponents.Find(comp => comp.Identifier == identifier);
+
+            // If the component is not null, show the component
+            component?.Show();
+        }
+
+        /// <summary>
+        /// Hides the specified component
+        /// </summary>
+        /// <param name="identifier"></param>
+        public void Hide(string identifier)
+        {
+            GUIComponent component = guiComponents.Find(comp => comp.Identifier == identifier);
+
+            component?.Hide();
+        }
+    }
+}
