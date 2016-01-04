d569a9406d88d21b1e599c7cb2131a9dacd54bc7
DevHalo <devhalo@hotmail.com>
2016-01-03 20:44:18 -0500

    Networking

---
+++ CStrike2DServer/Server.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+
+namespace CStrike2DServer
+{
+    class Server
+    {
+
+        static void Main(string[] args)
+        {
+            string serverVersion = "0.0.1a";            // Server Version
+            int port = 27015;
+            string serverName = "Global Offensive Server - " + serverVersion;
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("Global Offensive - Version " + serverVersion);
+            Console.WriteLine("==================================");
+
+            Console.WriteLine("Loading config file...");
+            ReadConfig();
+
+            Console.WriteLine("Booting up server...");
+
+            NetPeerConfiguration config = new NetPeerConfiguration("cstrike");
+            config.Port = port;
+            NetServer server = new NetServer(config);
+            server.Start();
+
+            Console.WriteLine("Server is live.");
+
+            NetIncomingMessage msg;
+            while ((msg = server.ReadMessage()) != null)
+            {
+                switch (msg.MessageType)
+                {
+                    case NetIncomingMessageType.Error:
+                        break;
+                    case NetIncomingMessageType.StatusChanged:
+                        break;
+                    case NetIncomingMessageType.ConnectionApproval:
+                        break;
+                    case NetIncomingMessageType.Data:
+                        Console.WriteLine(msg.ReadString());
+                        break;
+                    case NetIncomingMessageType.Receipt:
+                        break;
+                    case NetIncomingMessageType.DiscoveryRequest:
+                        break;
+                    case NetIncomingMessageType.DiscoveryResponse:
+                        break;
+                    case NetIncomingMessageType.VerboseDebugMessage:
+                        break;
+                    case NetIncomingMessageType.DebugMessage:
+                        break;
+                    case NetIncomingMessageType.WarningMessage:
+                        break;
+                    case NetIncomingMessageType.ErrorMessage:
+                        break;
+                    case NetIncomingMessageType.ConnectionLatencyUpdated:
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ReadConfig()
+        {
+            // If the config file exists, open it and process it
+            if (File.Exists("config.txt"))
+            {
+                string[] commmands = File.ReadAllLines("config.txt");
+
+
+                Console.WriteLine("Configuration Successfully Loaded.");
+            }
+            else
+            {
+                WriteFile();
+            }
+        }
+
+        /// <summary>
+        /// Writes a fresh config to the directory if none exists
+        /// </summary>
+        static void WriteFile()
+        {
+            Console.WriteLine("Configuration not found, creating default configuration...");
+            StreamWriter writer = File.CreateText("config.txt");
+
+            // Default settings
+
+            writer.Close();
+
+            Console.WriteLine("Configuration written to disk.");
+        }
+    }
+}
---
+++ cstrike2d/cstrike2d/NetworkManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+
+namespace CStrike2D
+{
+    public class NetworkManager
+    {
+        private string address;
+        private string clientVersion = "0.0.1a";
+        private int port = 27015;
+        private string clientName = "DevHalo";
+        private NetPeerConfiguration config;
+        private NetClient client;
+        private NetBuffer buffer;
+
+        private NetState curState;
+
+        public enum NetState
+        {
+            Disconnected,
+            Handshake,
+            Connected
+        }
+
+        public NetworkManager()
+        {
+            config = new NetPeerConfiguration("cstrike");
+            config.Port = port;
+            client = new NetClient(config);
+            curState = NetState.Disconnected;
+            buffer = new NetBuffer();
+        }
+
+
+        public void Connect(string address)
+        {
+            if (curState != NetState.Connected)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                client.Connect(address, port);
+
+                if (sw.Elapsed.Seconds < 5)
+                {
+                    if (client.ConnectionStatus == NetConnectionStatus.Connected)
+                    {
+                        NetOutgoingMessage msg = client.CreateMessage();
+                        msg.Write("HNDSHAKE" + clientName);
+                        client.SendMessage(msg, NetDeliveryMethod.ReliableOrdered);
+                        curState = NetState.Connected;
+                        sw.Stop();
+                    }
+                }
+            }
+        }
+
+        public void Update()
+        {
+            if (curState != NetState.Disconnected)
+            {
+                NetIncomingMessage msg = client.ReadMessage();
+                switch (curState)
+                {
+                    case NetState.Handshake:
+                        if (msg != null)
+                        {
+                            if (msg.MessageType == NetIncomingMessageType.Data)
+                            {
+                                if (msg.ReadString() == "accepted")
+                                {
+                                    curState = NetState.Connected;
+                                }
+                            }
+                        }
+                        break;
+                    case NetState.Connected:
+                        if (msg.MessageType != null)
+                        {
+
+                        }
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
