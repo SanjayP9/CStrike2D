12dbca7ed8ef618e272e55fe06841d9494dbceb1
DevHalo <devhalo@hotmail.com>
2016-01-04 02:49:53 -0500

    GameEngine development commenced

---
+++ cstrike2d/cstrike2d/Console.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrike2D
+{
+    static class Console
+    {
+
+    }
+
+    struct ConVar<T>
+    {
+        public T Value { get; private set; }
+        public string Command { get; private set; }
+
+        public ConVar(T value, string command)
+        {
+            Value = value;
+            Command = command;
+        } 
+    }
+}
---
+++ cstrike2d/cstrike2d/GameEngine.cs
@@ -0,0 +1,57 @@
+﻿// Author: Mark Voong
+// File Name: GameEngine.cs
+// Project Name: CStrike2D
+// Creation Date: Jan 4th, 2015
+// Modified Date: Jan 3rd, 2016
+// Description: Handles all logic and drawing of the in-game components
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CStrike2D
+{
+    class GameEngine
+    {
+        public List<Entity> Entities { get; private set; }
+        private CStrike2D driver;
+
+        public GameEngineState CurState { get; private set; }
+
+        public GameEngine(CStrike2D Driver)
+        {
+            Driver = driver;
+            CurState = GameEngineState.InActive;
+        }
+
+        public enum GameEngineState
+        {
+            InActive,
+            Active
+        }
+
+        public void AddEntity(Entity entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public void Update(float gameTime)
+        {
+            foreach (Entity entity in Entities)
+            {
+                entity.Update(gameTime);
+            }
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            foreach (Entity entity in Entities)
+            {
+                entity.Draw(sb);
+            }
+        }
+    }
+}
