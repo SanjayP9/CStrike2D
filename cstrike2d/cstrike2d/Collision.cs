66177d40d09a9c215a9b04590af8eae3070209be
shawnv98 <shawn.verma3@gmail.com>
2016-01-07 23:33:38 -0500

    Added Collision Class

---
+++ cstrike2d/cstrike2d/Collision.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrike2D
+{
+    class Collision
+    {
+        public bool PlayerToPlayer(Vector2 player1, Vector2 player2)
+        {
+
+            return false;
+        }
+    }
+}
