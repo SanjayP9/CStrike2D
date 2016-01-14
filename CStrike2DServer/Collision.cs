1ceac5fdad83df5e687c0985f56591c08a256f1a
shawnv98 <shawn.verma3@gmail.com>
2016-01-13 23:41:22 -0500

    Collision

--- CStrike2DServer/Collision.cs
+++ CStrike2DServer/Collision.cs
@@ -60,46 +60,57 @@ namespace CStrike2D
 
         public static bool LineRectangle(Rectangle enemyPlayer, float shotAngle, float mPlayer, float bPlayer)
         {
+            // Find centre and distance from centre to top right corner
             Vector2 centre = new Vector2(enemyPlayer.X + enemyPlayer.Width * 0.5f, enemyPlayer.Y - enemyPlayer.Height * 0.5f);
             float distance = Vector2.Distance(new Vector2(enemyPlayer.X, enemyPlayer.Y), centre);
 
+            // Holds the points of the rectangle before being rotated
             float x = enemyPlayer.X;
             float y = enemyPlayer.Y;
+
+            /////////////////////////////////
+            // FIND ROTATION OF EACH CORNER//
+            /////////////////////////////////
             Vector2 topLeft = new Vector2((float)(x * Math.Cos(shotAngle) - y * Math.Sin(shotAngle)) + centre.X,
                                           (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
 
             x = enemyPlayer.X + enemyPlayer.Width;
             y = enemyPlayer.Y;
             Vector2 topRight = new Vector2((float)(x * Math.Cos(shotAngle) - y * Math.Sin(shotAngle)) + centre.X,
-                                          (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
+                                           (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
 
             x = enemyPlayer.X;
-            y = enemyPlayer.Y - enemyPlayer.Height;
+            y = enemyPlayer.Y + enemyPlayer.Height;
             Vector2 bottomLeft = new Vector2((float)(x * Math.Cos(shotAngle) - y * Math.Sin(shotAngle)) + centre.X,
-                                          (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
+                                             (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
 
             x = enemyPlayer.X + enemyPlayer.Width;
-            y = enemyPlayer.Y - enemyPlayer.Height;
+            y = enemyPlayer.Y + enemyPlayer.Height;
             Vector2 bottomRight = new Vector2((float)(x * Math.Cos(shotAngle) - y * Math.Sin(shotAngle)) + centre.X,
-                                          (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
+                                              (float)(y * Math.Cos(shotAngle) + x * Math.Sin(shotAngle)) + centre.Y);
 
-
+            // Creates slope from top left to bottom right corner 
+            // Finds a P.O.I. with the shot angle and the line created
             float m1 = ((topLeft.Y - bottomRight.Y) / (topLeft.X - bottomRight.X));
             float b1 = centre.Y - m1 * centre.X;
             float poiX1 = (b1 - bPlayer) / (mPlayer - m1);
             float poiY1 = m1 * poiX1 + b1;
 
+            // Creates slope from top right to bottom left corner 
+            // Finds a P.O.I. with the shot angle and the line created
             float m2 = ((topRight.Y - bottomLeft.Y) / (topRight.X - bottomLeft.X));
             float b2 = centre.Y - m2 * centre.X;
             float poiX2 = (b2 - bPlayer) / (mPlayer - m2);
             float poiY2 = m2 * poiX1 + b2;
 
+            // If a P.O.I is within the rectangle return true
             if(Vector2.Distance(centre, new Vector2(poiX1,poiY1)) <= distance || 
                Vector2.Distance(centre, new Vector2(poiX2,poiY2)) <= distance)
             {
                 return true;
             }
 
+            // If a P.O.I is not within the rectangle return false
             return false;
         }
         
@@ -116,7 +127,6 @@ namespace CStrike2D
             {
                 return true; 
             }
-
 
             float cornerDistance_sq = (circleDistance.X - rect.Width / 2) * (circleDistance.X - rect.Width / 2) +
                                       (circleDistance.Y - rect.Height / 2) * (circleDistance.Y - rect.Height / 2);
