6ee56eef9b45ebe898c8c51d1938934f746b6a49
SanjayP9 <paraboosanjay@gmail.com>
2016-01-15 10:06:06 -0500

    ogsdihaflis

---
+++ cstrike2d/cstrike2d/ParticleEmitterView.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStrike2D
+{
+    class ParticleEmitterView
+    {
+        private ParticleEmitterModel model;
+
+        public ParticleEmitterView(ParticleEmitterModel model)
+        {
+            this.model = model;
+        }
+
+        public void Draw(SpriteBatch sb, Texture2D particleImg)
+        {
+            foreach(ParticleModel particle in model.Particles)
+            {
+                particle.View.Draw(sb, particleImg);
+            }
+        }
+    }
+}
