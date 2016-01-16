e2d761e8b8bb960c5f7523c2ddf2bf9ab38388d4
DevHalo <devhalo@hotmail.com>
2016-01-16 13:12:10 -0500

    Added client weapons

---
+++ cstrike2d/cstrike2d/ClientGrenade.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CStrike2D
+{
+    class ClientGrenade
+    {
+
+        public Vector2 Position { get; private set; }
+        private Vector2 velocity;
+
+        private ParticleEmitterModel grenadeEmitter;
+
+        public enum GrenadeType
+        {
+            Frag,
+            Flashbang,
+            Smoke,
+            Incendiary
+        }
+
+        public enum State
+        {
+            NotThrown,
+            Thrown,
+            Landed
+        }
+
+        /// <summary>
+        /// Throws the grenade
+        /// </summary>
+        /// <param name="angle"> Angle to throw the grenade from</param>
+        /// <param name="velocity"> If the player is moving, add this to the initial velocity </param>
+        public void Throw(float angle, Vector2 velocity)
+        {
+
+            // Get direction from vector
+            Vector2 direction = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+            
+            // Normalize the vector
+            direction.Normalize();
+
+            // If the player is already moving, this will increase the initial velocity of the
+            // grenade.
+            direction += velocity;
+
+            // Set the velocity of this grenade
+            this.velocity = direction;
+        }
+
+        /// <summary>
+        /// If the grenade collides with the world, 
+        /// </summary>
+        public void CollidedWithWorld()
+        {
+            
+        }
+        
+
+        public void Update(float gameTime)
+        {
+            
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            
+        }
+    }
+}
---
+++ cstrike2d/cstrike2d/ClientWeapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CStrike2D
+{
+    class ClientWeapon
+    {
+
+        public ClientPlayer Owner { get; private set; }
+        public ParticleEmitterModel GunEmitter { get; private set; }
+        public bool Fired { get; private set; }
+
+        public Vector2 Position { get; private set; }
+        public float Rotation { get; private set; }
+
+        public void FireWeapon()
+        {
+            if (!Fired)
+            {
+                Fired = true;
+            }
+        }
+
+
+        public void Update(float gameTime)
+        {
+            // If the weapon is currently being "held" by a player,
+            // if null, it is on the floor
+            if (Owner != null)
+            {
+                Position = Owner.Position;
+                Rotation = Owner.Rotation;
+            }
+        }
+
+        /// <summary>
+        /// Drops the weapon 
+        /// </summary>
+        public void Drop()
+        {
+            Owner = null;
+        }
+
+
+        public void Draw(SpriteBatch sb)
+        {
+            // Draw particles
+        }
+    }
+}
